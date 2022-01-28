c3a5823773a292f8aea9863ffdb4059f535e5a70
BrazhnikovJee <vbrazhnikoff.jee@gmail.com>
2022-01-28 17:38:00 +0300

    Separation by direction

--- Script_HW_TerminalLinux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd C:/Users/Vadim/Desktop/test/test1
-mkdir lesson work class
-cd /c/Users/Vadim/Desktop/test/work
-touch 1.txt 2.txt 3.txt one.json two.json
-mkdir miron roma petr
-ls
-mv one.json two.json roma

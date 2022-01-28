bff8cd31ef77b121895bf77df74b09e093e9df70
BrazhnikovJee <vbrazhnikoff.jee@gmail.com>
2022-01-28 17:35:35 +0300

    HomeWork

---
+++ TerminalLinux/Script_HW_TerminalLinux.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd C:/Users/Vadim/Desktop/test/test1
+mkdir lesson work class
+cd /c/Users/Vadim/Desktop/test/work
+touch 1.txt 2.txt 3.txt one.json two.json
+mkdir miron roma petr
+ls
+mv one.json two.json roma
